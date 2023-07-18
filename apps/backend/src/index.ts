54f8e3a82d7673d61c46d110046e8bcba8f98ac5
Karol <karolpiskorz8@gmail.com>
2023-07-18 14:12:05 +0200

    Fix env errors

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,5 @@
-import { corsConfig } from "./config/cors";
 import "./config/env";
+import { corsConfig } from "./config/cors";
 import express from "express";
 import routes from "./routes";
 import cors from "cors";
@@ -14,6 +14,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000);
+const PORT = process.env["PORT"] || 4000;
+
+app.listen(PORT);
 
 console.log("app is running.");
