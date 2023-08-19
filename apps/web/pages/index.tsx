283e485e632e75b26780f8eb761bf3d0ce15d30a
Karol Piskorz <karolpiskorz8@gmail.com>
2023-08-19 14:28:31 +0200

    Add create page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,61 +3,20 @@ import { useCreatePoll } from "../hooks/use-create-poll";
 import { Alert } from "@mui/material";
 import { getErrorMessage } from "../utils/error";
 import type { Poll } from "types";
+import { useRouter } from "next/router";
 
 type FormValues = Poll.CreatePollData;
 
 export default function Page() {
-  const {
-    handleSubmit,
-    control,
-    register,
-    setError,
-    reset,
-    formState: { errors },
-  } = useForm<FormValues>({ defaultValues: { answers: [{ text: "" }] } });
-  const { fields, append } = useFieldArray({
-    control,
-    name: "answers",
-  });
-  const { mutateAsync } = useCreatePoll();
-
-  const onSubmit = handleSubmit(async (data: FormValues) => {
-    try {
-      console.log(data);
-      const response = await mutateAsync(data);
-      console.log(response);
-      reset();
-    } catch (error) {
-      setError("root", { message: getErrorMessage(error) });
-    }
-  });
-
+  const router = useRouter();
   return (
     <>
-      <form className="flex flex-col space-y-2" onSubmit={onSubmit}>
-        {errors.root?.message ? (
-          <Alert severity="error">{errors.root.message}</Alert>
-        ) : null}
-        <input
-          className="text-2xl"
-          type="text"
-          placeholder="Your question..."
-          {...register("question")}
-        />
-        {fields.map((field, index) => (
-          <input
-            className="border border-black px-4 py-2 w-full"
-            key={field.id}
-            {...register(`answers.${index}.text` as const)}
-            placeholder={`Answer ${index}`}
-          />
-        ))}
-
-        <button type="button" onClick={() => append({ text: "" })}>
-          Add new option
-        </button>
-        <button type="submit">Submit</button>
-      </form>
+      <button
+        className="border border-black p-2 px-4"
+        onClick={() => router.push("/create")}
+      >
+        Create Poll
+      </button>
     </>
   );
 }
