98392a3539d788e98eae16b0eae8dc9063f09c55
Karol Piskorz <karolpiskorz8@gmail.com>
2023-08-20 17:33:24 +0200

    Add RadioGroup component

--- apps/web/pages/[pollId].tsx
+++ apps/web/pages/[pollId].tsx
@@ -2,6 +2,12 @@ import { useRouter } from "next/router";
 import React from "react";
 import { useGetPoll } from "../hooks/use-get-poll";
 import { CircularProgress, LinearProgress } from "@mui/material";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { RadioGroup, RadioGroupItem } from "../components/radio-group";
+
+type FormValues = {};
 
 const PollPage = () => {
   const router = useRouter();
@@ -19,22 +25,38 @@ const PollPage = () => {
       .map((answer) => answer.votes)
       .reduce((prev, next) => prev + next) || 0;
 
+  const form = useForm<FormValues>();
+
+  function onSubmit(data: FormValues) {
+    console.log(data);
+  }
+
   if (isLoading) return <CircularProgress />;
   return (
     <>
       <p>Poll: {pollId}</p>
       <p>Error: {JSON.stringify(error)}</p>
-      {isSuccess &&
-        data.answers.map((answer) => (
-          <div className="border border-black py-2 px-4" key={answer.id}>
-            {answer.text} {answer.votes}/{maxVotes} ({calcPercent(answer.votes)}
-            %)
-            <LinearProgress
-              variant="determinate"
-              value={calcPercent(answer.votes)}
-            />
-          </div>
-        ))}
+      {isSuccess && (
+        <RadioGroup className="flex flex-col space-y-1">
+          {data.answers.map((answer) => (
+            <div className="border border-black py-2 px-4" key={answer.id}>
+              <div className="flex items-center space-x-3 space-y-0">
+                <div>
+                  <RadioGroupItem value={answer.text} />
+                </div>
+                <label className="font-normal">{answer.text}</label>
+              </div>
+              {answer.text} {answer.votes}/{maxVotes} (
+              {calcPercent(answer.votes)}
+              %)
+              <LinearProgress
+                variant="determinate"
+                value={calcPercent(answer.votes)}
+              />
+            </div>
+          ))}
+        </RadioGroup>
+      )}
     </>
   );
 };
