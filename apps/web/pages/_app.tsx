da34b97450aca7cf353232cde71d428a642afb00
Karol Piskorz <karolpiskorz8@gmail.com>
2023-08-23 18:47:21 +0200

    Add react-query-devtools

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import type { AppProps } from "next/app";
 import {
   Hydrate,
@@ -6,13 +6,37 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import "../globals.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+  const [showDevtools, setShowDevtools] = useState(false);
+
+  useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+        {showDevtools && (
+          <React.Suspense fallback={null}>
+            <ReactQueryDevtoolsProduction />
+          </React.Suspense>
+        )}
       </Hydrate>
     </QueryClientProvider>
   );
