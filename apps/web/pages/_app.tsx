324c4d7c97f10d80e4c7c3b1dd71be3faa34703a
Karol Piskorz <karolpiskorz8@gmail.com>
2023-08-25 12:57:50 +0200

    Add font config for tailwindcss

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -10,7 +10,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClientConfig } from "../config/query-client";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 const ReactQueryDevtoolsProduction = React.lazy(() =>
   import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
@@ -31,14 +31,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <style jsx global>{`
-        html {
-          font-family: ${inter.style.fontFamily};
-        }
-      `}</style>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
+          <main className={`${inter.variable} font-sans`}>
+            <Component {...pageProps} />
+          </main>
           <ReactQueryDevtools initialIsOpen={false} />
           {showDevtools && (
             <React.Suspense fallback={null}>
