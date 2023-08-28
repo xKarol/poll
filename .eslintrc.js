332e59a964dd1459efd172501325b91b3f6f42b0
Karol Piskorz <karolpiskorz8@gmail.com>
2023-08-28 16:38:03 +0200

    Add global prettier file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
   settings: {
     next: {
---
+++ packages/prettier-config/prettier-preset.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  semi: true,
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: "auto",
+  trailingComma: "es5",
+  arrowParens: "always",
+};
