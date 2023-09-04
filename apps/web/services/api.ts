cfaf4f3935f7fdac4a1b721fef6c9ba78372283a
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-04 17:15:20 +0200

    Fix types

--- apps/web/services/api.ts
+++ apps/web/services/api.ts
@@ -2,15 +2,12 @@ import type { Poll } from "@poll/types";
 
 import axios from "../lib/axios";
 
-export const createPoll: Poll.Services["createPoll"] = async (pollData) => {
+export const createPoll: Poll.Api["createPoll"] = async (pollData) => {
   const { data } = await axios.post("/poll", pollData);
   return data;
 };
 
-export const getPolls: Poll.Services["getPolls"] = async (
-  page = 1,
-  limit = 10
-) => {
+export const getPolls: Poll.Api["getPolls"] = async (page = 1, limit = 10) => {
   const { data } = await axios.get(`/poll`, {
     params: {
       page,
@@ -20,12 +17,12 @@ export const getPolls: Poll.Services["getPolls"] = async (
   return data;
 };
 
-export const getPoll: Poll.Services["getPoll"] = async (pollId) => {
+export const getPoll: Poll.Api["getPoll"] = async (pollId) => {
   const { data } = await axios.get(`/poll/${pollId}`);
   return data;
 };
 
-export const votePoll: Poll.Services["votePoll"] = async (pollId, answerId) => {
+export const votePoll: Poll.Api["votePoll"] = async (pollId, answerId) => {
   const { data } = await axios.post(`/poll/${pollId}/vote/${answerId}`);
   return data;
 };
--- packages/types/poll.d.ts
+++ packages/types/poll.d.ts
@@ -14,12 +14,6 @@ export type GetPollData = {
   pollId: string;
 };
 
-export type GetPollsData = {
-  page?: number;
-  skip: number;
-  limit?: number;
-};
-
 export type VotePollData = {
   pollId: string;
   answerId: string;
@@ -30,7 +24,24 @@ export type PaginationResult<T> = {
   nextPage: number | undefined;
 };
 
+// Backend
 export type Services = {
+  getPoll: (pollId: string) => Promise<Poll & { answers: Answer[] }>;
+  getPolls: (params: {
+    page?: number;
+    skip: number;
+    limit?: number;
+  }) => Promise<PaginationResult<(Poll & { totalVotes: number })[]>>;
+  createPoll: (pollData: CreatePollData) => Promise<Poll>;
+  deletePoll: (pollId: string) => Promise<void>;
+  votePoll: (
+    pollId: string,
+    answerId: string
+  ) => Promise<Poll & { answers: Answer[] }>;
+};
+
+// Frontend
+export type Api = {
   getPoll: (pollId: string) => Promise<Poll & { answers: Answer[] }>;
   getPolls: (
     page?: number,
