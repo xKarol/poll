79195a758785a1ca03138ab6d9cdac72a3acb876
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-06 15:44:32 +0200

    Add stats section

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,25 @@
+import prisma from "@poll/prisma";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 import { routes } from "../config/routes";
 
-export default function Page() {
+type Stats = {
+  totalPolls: number;
+  totalVotes: number;
+  totalUsers: number;
+};
+
+export async function getServerSideProps() {
+  const stats = {
+    totalPolls: await prisma.poll.count(),
+    totalVotes: 0, // todo await prisma.vote.count(),
+    totalUsers: await prisma.user.count(),
+  };
+  return { props: { stats } };
+}
+
+export default function Page({ stats }: { stats: Stats }) {
   const { data: session } = useSession();
   return (
     <>
@@ -33,6 +49,20 @@ export default function Page() {
         </Link>
       )}
       <pre>{JSON.stringify(session, null, 2)}</pre>
+      <section className="flex space-x-5">
+        <div>
+          <p>Total Polls</p>
+          <span>{stats.totalPolls}</span>
+        </div>
+        <div>
+          <p>Total Votes</p>
+          <span>{stats.totalVotes}</span>
+        </div>
+        <div>
+          <p>Total Users</p>
+          <span>{stats.totalUsers}</span>
+        </div>
+      </section>
     </>
   );
 }
