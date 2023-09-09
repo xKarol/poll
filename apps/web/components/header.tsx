b03af7df4bba90ffd41c351b9e2013526c58cf27
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-09 13:28:00 +0200

    Add header nav items

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -2,9 +2,31 @@ import { Avatar } from "@mui/material";
 import { cn } from "@poll/lib";
 import { Logo } from "@poll/ui";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import React from "react";
 
+import { routes } from "../config/routes";
+
 type HeaderProps = React.ComponentPropsWithoutRef<"header">;
+
+const navLinks = [
+  {
+    text: "Create Poll",
+    href: routes.CREATE_POLL,
+  },
+  {
+    text: "Public Polls",
+    href: routes.PUBLIC_POLLS,
+  },
+  {
+    text: "Features",
+    href: routes.HOME, //TODO change
+  },
+  {
+    text: "Pricing",
+    href: routes.HOME, //TODO change
+  },
+];
 
 const Header = ({ className, ...rest }: HeaderProps) => {
   const { data: session } = useSession();
@@ -17,17 +39,33 @@ const Header = ({ className, ...rest }: HeaderProps) => {
       )}
       {...rest}>
       <Logo />
-      <HamburgerMenu />
-      {session ? (
-        <div className="flex justify-center space-x-2 items-center">
-          <Avatar
-            src={session.user.image}
-            sx={{ width: "32px", height: "32px" }}>
-            {session.user.name[0]}
-          </Avatar>
-          <span>{session.user.name}</span>
-        </div>
-      ) : null}
+      <nav className="hidden xl:flex space-x-12 items-center">
+        <ul className="flex space-x-12 font-semibold">
+          {navLinks.map((link) => (
+            <li key={link.text}>
+              <Link href={link.href}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+
+        {session ? (
+          <div className="flex justify-center space-x-2 items-center">
+            <Avatar
+              src={session.user.image}
+              sx={{ width: "32px", height: "32px" }}>
+              {session.user.name[0]}
+            </Avatar>
+            <span>{session.user.name}</span>
+          </div>
+        ) : (
+          <Link
+            href={routes.LOGIN}
+            className="p-2 px-4 bg-green-500 rounded-md text-neutral-900 font-medium">
+            Login
+          </Link>
+        )}
+      </nav>
+      <HamburgerMenu className="xl:hidden" />
     </header>
   );
 };
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -28,31 +28,14 @@ export default function Page({ stats }: { stats: Stats }) {
   return (
     <>
       <Header />
-      <Link className="border border-black p-2 px-4" href={routes.CREATE_POLL}>
-        Create Poll
-      </Link>
-      <Link className="border border-black p-2 px-4" href={routes.PUBLIC_POLLS}>
-        Public Polls
-      </Link>
 
       {session ? (
-        <>
-          <Link
-            className="border border-black p-2 px-4"
-            href={routes.USER_POLLS}>
-            Your Polls
-          </Link>
-          <button
-            className="border border-black p-2 px-4"
-            onClick={() => signOut()}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <Link className="border border-black p-2 px-4" href={routes.LOGIN}>
-          Login
-        </Link>
-      )}
+        <button
+          className="border border-black p-2 px-4"
+          onClick={() => signOut()}>
+          Logout
+        </button>
+      ) : null}
       <pre>{JSON.stringify(session, null, 2)}</pre>
       <section className="flex space-x-5">
         <div>
