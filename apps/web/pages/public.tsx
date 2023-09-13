86207d69a0fcdffb309368c6b9627925f25abfc5
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-13 09:58:20 +0200

    Fix public page dark mode colors

--- apps/web/pages/public.tsx
+++ apps/web/pages/public.tsx
@@ -29,7 +29,7 @@ export default function Page() {
       <div className="space-y-8 container mt-8 lg:mt-16 md:max-w-2xl xl:max-w-4xl">
         <div className="justify-between items-center flex">
           <h1 className="text-2xl font-medium">Public Polls</h1>
-          <Button className="text-neutral-900 bg-neutral-100 hover:bg-neutral-100/50">
+          <Button className="text-neutral-900 bg-neutral-100 hover:bg-neutral-100/50 dark:bg-neutral-800 dark:text-white">
             Recent <Icon.ChevronDown />
           </Button>
         </div>
@@ -43,9 +43,9 @@ export default function Page() {
                 <Link
                   key={poll.id}
                   href={routes.poll(poll.id)}
-                  className="bg-neutral-100 px-3 py-4 rounded-[4px] space-y-4">
+                  className="bg-neutral-100 dark:bg-neutral-800 px-3 py-4 rounded-[4px] space-y-4">
                   <h1 className="text-lg">{poll.question}</h1>
-                  <div className="flex justify-between items-center text-sm font-normal text-neutral-300">
+                  <div className="flex justify-between items-center text-sm font-normal text-neutral-300 dark:text-neutral-400">
                     <span>{dayjs(poll.createdAt).fromNow()}</span>
                     <span>{poll.totalVotes} votes</span>
                   </div>
