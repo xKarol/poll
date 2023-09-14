41253d907898d0ae7a395ea5de23f428e6c60a6a
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-14 20:22:15 +0200

    Fix loading env variables during test

--- apps/backend/src/config/env.ts
+++ apps/backend/src/config/env.ts
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
+import path from "path";
 
 const isProduction = process.env.NODE_ENV === "production";
+const isTest = process.env.NODE_ENV === "test";
 
 if (isProduction) {
   dotenv.config();
+} else if (isTest) {
+  dotenv.config({ path: path.join(__dirname, "../../.env.test") });
 } else {
   dotenv.config({ path: "./.env.dev" });
 }
