12abe85b17d651d3ac1908d4d8b5671720745fcd
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-17 18:10:48 +0200

    Using constant data for pricing plans

--- apps/web/pages/pricing.tsx
+++ apps/web/pages/pricing.tsx
@@ -1,20 +1,32 @@
 import { LoadingButton } from "@poll/ui";
-import { useMutation, useQuery } from "@tanstack/react-query";
+// import { useMutation, useQuery } from "@tanstack/react-query";
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
 
 import Header from "../components/header";
-import axios from "../lib/axios";
+
+// import axios from "../lib/axios";
+
+const pricingPlans = [
+  {
+    priceId: "test_eVa9CM02gfUGdDqfYZ",
+    name: "Premium Plan",
+  },
+  {
+    priceId: "test_eVacOY7uI23Q2YMfYY",
+    name: "Pro Plan",
+  },
+];
 
 export default function Page() {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["payments-list"],
-    queryFn: () => axios.get("/payments"),
-  });
-  const { mutateAsync } = useMutation({
-    // @ts-ignore TODO
-    mutationFn: ({ priceId }) => axios.post("/payments", { priceId }),
-  });
+  // const { data, isLoading, isError } = useQuery({
+  //   queryKey: ["payments-list"],
+  //   queryFn: () => axios.get("/payments"),
+  // });
+  // const { mutateAsync } = useMutation({
+  //   // @ts-ignore TODO
+  //   mutationFn: ({ priceId }) => axios.post("/payments", { priceId }),
+  // });
   const router = useRouter();
 
   return (
@@ -23,26 +35,21 @@ export default function Page() {
       <NextSeo title="Pricing" />
       <main className="container">
         <div className="flex flex-col">
-          {!isLoading &&
-            !isError &&
-            data.data.map((price) => (
-              <div key={price.id}>
-                {price.unit_amount}
-                {price.currency}
-                <LoadingButton
-                  isLoading={false}
-                  type="button"
-                  onClick={async () => {
-                    // @ts-ignore
-                    const { data: url } = await mutateAsync({
-                      priceId: price.id,
-                    });
-                    router.push(url);
-                  }}>
-                  Buy
-                </LoadingButton>
-              </div>
-            ))}
+          {pricingPlans.map((price) => (
+            <div key={price.priceId}>
+              {price.name}
+              {/* {price.unit_amount}
+              {price.currency} */}
+              <LoadingButton
+                isLoading={false}
+                type="button"
+                onClick={() => {
+                  router.push(`https://buy.stripe.com/${price.priceId}`);
+                }}>
+                Buy
+              </LoadingButton>
+            </div>
+          ))}
         </div>
       </main>
     </>
