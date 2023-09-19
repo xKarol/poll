b7932e63c9a346b943808d57607bf2504e479567
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-19 16:50:10 +0200

    Catching errors for /webhook/stripe endpoint

--- apps/backend/src/routes/webhooks.ts
+++ apps/backend/src/routes/webhooks.ts
@@ -8,27 +8,34 @@ const router = express.Router();
 router.post(
   "/webhook/stripe",
   express.raw({ type: "application/json" }),
-  async (request, response) => {
-    const signature = request.headers["stripe-signature"];
-    const event = stripe.webhooks.constructEvent(
-      request.body,
-      signature as string,
-      process.env.STRIPE_WEBHOOK_SECRET as string
-    );
-    switch (event.type) {
-      case "payment_intent.succeeded": {
-        // @ts-expect-error
-        const { userId, planName } = event.data.object.metadata as {
-          userId: string;
-          planName: Plan;
-        };
+  async (request, response, next) => {
+    try {
+      const signature = request.headers["stripe-signature"];
+      const event = stripe.webhooks.constructEvent(
+        request.body,
+        signature as string,
+        process.env.STRIPE_WEBHOOK_SECRET as string
+      );
+      switch (event.type) {
+        case "payment_intent.succeeded": {
+          // @ts-expect-error
+          const { userId, planName } = event.data.object.metadata as {
+            userId: string;
+            planName: Plan;
+          };
 
-        prisma.user.update({ where: { id: userId }, data: { plan: planName } });
-        break;
+          prisma.user.update({
+            where: { id: userId },
+            data: { plan: planName },
+          });
+          break;
+        }
       }
-    }
 
-    response.json({ received: true });
+      response.json({ received: true });
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
