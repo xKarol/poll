2490e81b25a97d160fc5a48d4195b0b8d8f99452
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-20 18:44:48 +0200

    Add stats section

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -23,6 +23,9 @@ export async function getServerSideProps() {
 export default function Page({ stats }: { stats: Stats }) {
   const { data: session } = useSession();
   const { setTheme, theme } = useTheme();
+
+  console.log({ session });
+
   return (
     <>
       <Header />
@@ -35,18 +38,35 @@ export default function Page({ stats }: { stats: Stats }) {
         </button>
       ) : null}
       <pre>{JSON.stringify(session, null, 2)}</pre>
-      <section className="flex space-x-5">
-        <div>
-          <p>Total Polls</p>
-          <span>{stats.totalPolls}</span>
-        </div>
-        <div>
-          <p>Total Votes</p>
-          <span>{stats.totalVotes}</span>
-        </div>
-        <div>
-          <p>Total Users</p>
-          <span>{stats.totalUsers}</span>
+      <section className="border-b-2 border-t-2 border-neutral-800 bg-neutral-800/25">
+        <div className="container flex max-w-4xl flex-col items-center space-y-4 py-8 md:flex-row md:justify-between md:space-x-4 md:space-y-0">
+          <div className="flex flex-1 flex-col items-center">
+            <p className="text-4xl font-bold uppercase md:text-5xl">
+              {stats.totalPolls}
+            </p>
+            <p className="text-base font-semibold uppercase md:text-lg">
+              Polls
+            </p>
+          </div>
+          <div className="hidden h-[50px] w-[1px] bg-neutral-800 md:block"></div>
+          <div className="flex flex-1 flex-col items-center">
+            <p className="text-4xl font-bold uppercase md:text-5xl">
+              {stats.totalVotes}
+            </p>
+            <p className="text-base font-semibold uppercase md:text-lg">
+              Votes
+            </p>
+          </div>
+          <div className="hidden h-[50px] w-[1px] bg-neutral-800 md:block"></div>
+
+          <div className="flex flex-1 flex-col items-center">
+            <p className="text-4xl font-bold uppercase md:text-5xl">
+              {stats.totalUsers}
+            </p>
+            <p className="text-base font-semibold uppercase md:text-lg">
+              Users
+            </p>
+          </div>
         </div>
       </section>
       <MoonIcon
