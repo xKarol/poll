88de60251d70e21eb213cac79afe6fbfdd45c1bc
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-21 20:05:53 +0200

    Change button disabled opacity value

--- packages/ui/components/button/button.tsx
+++ packages/ui/components/button/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import React from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-[4px] text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-[4px] text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-80",
   {
     variants: {
       variant: {
