99516a965677df4bdecb01b67e824a3b27e31aa5
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-24 20:12:14 +0200

    Fix return type

--- apps/web/utils/error.ts
+++ apps/web/utils/error.ts
@@ -2,7 +2,7 @@ import { AxiosError } from "axios";
 import { ZodError } from "zod";
 
 // TODO make global utils and reuse between backend and frontend
-export const getErrorMessage = (error: unknown) => {
+export const getErrorMessage = (error: unknown): string => {
   if (error instanceof ZodError) return error.issues[0].message;
   if (error instanceof AxiosError)
     return error.response?.data?.message || "Unknown error";
