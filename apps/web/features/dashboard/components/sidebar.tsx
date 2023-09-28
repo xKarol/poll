5de4a758697d46707ff10392817b020e4398638e
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-28 20:36:17 +0200

    Add sidebar user component

--- apps/web/features/dashboard/components/sidebar.tsx
+++ apps/web/features/dashboard/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from "@mui/material";
 import { cn } from "@poll/lib";
 import { Icon } from "@poll/ui";
 import { signOut } from "next-auth/react";
@@ -37,27 +38,30 @@ const Sidebar = ({ className, ...props }: Props) => {
   return (
     <aside
       className={cn(
-        "flex h-screen w-[220px] flex-col justify-between border-r border-neutral-200/50 bg-neutral-100 px-2 py-4 dark:border-neutral-800/50 dark:bg-neutral-800/25",
+        "flex h-screen w-[220px] flex-col border-r border-neutral-200/50 bg-neutral-100 px-2 py-4 dark:border-neutral-800/50 dark:bg-neutral-800/25",
         className
       )}
       {...props}>
-      <nav className="space-y-[0.125rem]">
-        {sidebarLinks.map((link) => (
-          <SidebarNavigationLink
-            key={link.text}
-            as={Link}
-            href={link.href}
-            IconElement={link.IconElement}
-            isActive={router.asPath === link.href}>
-            {link.text}
-          </SidebarNavigationLink>
-        ))}
-      </nav>
-      <SidebarNavigationLink
-        IconElement={<Icon.LogOut />}
-        onClick={() => signOut()}>
-        Log Out
-      </SidebarNavigationLink>
+      <SidebarUser username="John Doe" className="mb-6" />
+      <div className="flex flex-1 flex-col justify-between">
+        <nav className="space-y-[0.125rem]">
+          {sidebarLinks.map((link) => (
+            <SidebarNavigationLink
+              key={link.text}
+              as={Link}
+              href={link.href}
+              IconElement={link.IconElement}
+              isActive={router.asPath === link.href}>
+              {link.text}
+            </SidebarNavigationLink>
+          ))}
+        </nav>
+        <SidebarNavigationLink
+          IconElement={<Icon.LogOut />}
+          onClick={() => signOut()}>
+          Log Out
+        </SidebarNavigationLink>
+      </div>
     </aside>
   );
 };
@@ -95,5 +99,34 @@ export function SidebarNavigationLink<T extends React.ElementType = "button">({
       {IconElement}
       <span>{children}</span>
     </Component>
+  );
+}
+
+export type SidebarUserProps = {
+  avatarUrl?: string;
+  username: string;
+} & React.ComponentPropsWithoutRef<"div">;
+
+export function SidebarUser({
+  avatarUrl,
+  username,
+  className,
+  ...props
+}: SidebarUserProps) {
+  return (
+    <div
+      className={cn(
+        "flex cursor-pointer items-center space-x-2 rounded-[4px] p-2 text-sm transition-colors hover:bg-neutral-200 hover:dark:bg-neutral-800",
+        className
+      )}
+      {...props}>
+      <Avatar sx={{ width: 20, height: 20 }} src={avatarUrl}>
+        {username[0]}
+      </Avatar>
+      <div className="flex items-center space-x-2">
+        <span>{username}</span>
+        <Icon.ChevronDown className="h-4 w-4" />
+      </div>
+    </div>
   );
 }
