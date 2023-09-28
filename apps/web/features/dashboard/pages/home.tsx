ed3e9da4905a382566f8c2662073b8d7c3536a3f
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-28 20:16:30 +0200

    Add loading indicator for home page (autoredirect)

--- apps/web/features/dashboard/pages/home.tsx
+++ apps/web/features/dashboard/pages/home.tsx
@@ -1,17 +1,12 @@
-import Link from "next/link";
+import { Icon } from "@poll/ui";
 import React from "react";
 
-import { routes } from "../../../config/routes";
 import { BaseLayout } from "../layouts";
 
 const HomePage = () => {
   return (
     <BaseLayout>
-      <h1 className="mb-5">Dashboard</h1>
-      <div className="flex flex-col space-y-5">
-        <Link href={routes.DASHBOARD.POLLS}>Polls</Link>
-        <Link href={routes.HOME}>Settings</Link>
-      </div>
+      <Icon.Loader2 className="m-auto animate-spin" />
     </BaseLayout>
   );
 };
