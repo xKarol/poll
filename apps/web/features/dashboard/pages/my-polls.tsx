12214fb9a2d357e3e1b430ccc3d9104402ff8b3b
Karol Piskorz <karolpiskorz8@gmail.com>
2023-09-29 17:23:27 +0200

    Fix user polls state displaying

--- apps/web/features/dashboard/pages/my-polls.tsx
+++ apps/web/features/dashboard/pages/my-polls.tsx
@@ -14,6 +14,7 @@ const MyPollsPage = () => {
     data: pages,
     isLoading,
     isFetchingNextPage,
+    isSuccess,
     error,
     hasNextPage,
     fetchNextPage,
@@ -32,47 +33,48 @@ const MyPollsPage = () => {
         </Button>
       </div>
       <div className="flex flex-col">
-        <div className="mb-2 flex px-4 py-2 [&>*]:max-w-[150px] [&>*]:flex-1">
-          <span className="!max-w-none space-x-2">Question</span>
-          <div>
-            <div className="flex items-center space-x-2">
-              <span>Votes</span>
-            </div>
-          </div>
-          <div>
-            <div className="flex items-center space-x-2">
-              <span>Public</span>
-            </div>
-          </div>
-          <span className="space-x-2"></span>
-        </div>
         {error && <div>{getErrorMessage(error)}</div>}
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <div className="flex flex-col space-y-2">
-            {data?.map((poll) => (
-              <Link
-                key={poll.id}
-                href={routes.poll(poll.id)}
-                className="flex items-center justify-between rounded-[4px] border border-neutral-700/50 px-4 py-2 text-sm transition-colors hover:bg-neutral-700/5 [&_*]:max-w-[150px] [&_*]:flex-1">
-                <span className="!max-w-none">{poll.question}</span>
-                <span>{poll.totalVotes}</span>
-                <div className="">
-                  <span className="rounded-[2px] bg-neutral-800 p-1 text-xs">
-                    {poll.isPublic ? "Public" : "Private"}
-                  </span>
+        {isLoading && <Icon.Loader2 className="m-auto animate-spin" />}
+        {isSuccess && (
+          <>
+            <div className="mb-2 flex px-4 py-2 [&>*]:max-w-[150px] [&>*]:flex-1">
+              <span className="!max-w-none space-x-2">Question</span>
+              <div>
+                <div className="flex items-center space-x-2">
+                  <span>Votes</span>
                 </div>
-                <Icon.MoreHorizontal />
-              </Link>
-            ))}
-            <div className="mx-auto">
-              {isFetchingNextPage && <Loader />}
-              {hasNextPage && (
-                <button onClick={() => fetchNextPage()}>Fetch More</button>
-              )}
+              </div>
+              <div>
+                <div className="flex items-center space-x-2">
+                  <span>Public</span>
+                </div>
+              </div>
+              <span className="space-x-2"></span>
             </div>
-          </div>
+            <div className="flex flex-col space-y-2">
+              {data?.map((poll) => (
+                <Link
+                  key={poll.id}
+                  href={routes.poll(poll.id)}
+                  className="flex items-center justify-between rounded-[4px] border border-neutral-700/50 px-4 py-2 text-sm transition-colors hover:bg-neutral-700/5 [&_*]:max-w-[150px] [&_*]:flex-1">
+                  <span className="!max-w-none">{poll.question}</span>
+                  <span>{poll.totalVotes}</span>
+                  <div className="">
+                    <span className="rounded-[2px] bg-neutral-800 p-1 text-xs">
+                      {poll.isPublic ? "Public" : "Private"}
+                    </span>
+                  </div>
+                  <Icon.MoreHorizontal />
+                </Link>
+              ))}
+              <div className="mx-auto">
+                {isFetchingNextPage && <Loader />}
+                {hasNextPage && (
+                  <button onClick={() => fetchNextPage()}>Fetch More</button>
+                )}
+              </div>
+            </div>
+          </>
         )}
       </div>
     </BaseLayout>
