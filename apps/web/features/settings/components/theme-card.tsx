d262103b13cced4a164820053616cfeb00053fe8
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-01 10:49:15 +0200

    Checking theme card radio input when user click on the box

--- apps/web/features/settings/components/theme-card.tsx
+++ apps/web/features/settings/components/theme-card.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@poll/lib";
 import Image from "next/image";
-import React, { useId } from "react";
+import React, { useId, useRef } from "react";
 
 import { Label } from "../../../components/label";
 import darkThemePreview from "../../../public/dark-theme-preview.svg";
@@ -27,19 +27,32 @@ function ThemeCard({
   ...props
 }: Props) {
   const id = useId();
+  const radioRef = useRef(null);
+
+  const triggerCheck = () => {
+    if (radioRef.current) {
+      radioRef.current.click();
+    }
+  };
+
   return (
     <div
       className={cn(
-        "rounded-[4px] border border-neutral-200 dark:border-neutral-700/50",
+        "cursor-pointer rounded-[4px] border border-neutral-200 dark:border-neutral-700/50",
         isActive && "outline outline-2 outline-neutral-900 dark:outline-white",
         className
       )}
+      onClick={triggerCheck}
       {...props}>
       <div className="flex h-[200px] w-full items-center justify-center bg-neutral-200 px-6 py-4 dark:bg-neutral-800">
-        <Image src={variantPreview[variant]} alt={`${variant} theme preview`} />
+        <Image
+          src={variantPreview[variant]}
+          alt={`${variant} theme preview`}
+          draggable={false}
+        />
       </div>
       <div className="flex items-center space-x-2 p-4 text-sm">
-        {React.cloneElement(RadioInput, { id })}
+        {React.cloneElement(RadioInput, { id, ref: radioRef })}
         <Label className="text-sm capitalize" htmlFor={id}>
           {variant} theme
         </Label>
