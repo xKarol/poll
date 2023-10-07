aabb439704601a927fb01f5b30d9bd974f5c5951
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-07 15:40:48 +0200

    Add icons for the input component

--- packages/ui/components/input/input.tsx
+++ packages/ui/components/input/input.tsx
@@ -2,20 +2,27 @@ import { cn } from "@poll/lib";
 import * as React from "react";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  LeftIcon?: JSX.Element;
+  RightIcon?: JSX.Element;
+}
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ LeftIcon, RightIcon, className, type, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "ring-offset-background flex h-10 w-full rounded-sm border-2 border-neutral-100 bg-white px-4 py-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 disabled:cursor-not-allowed disabled:opacity-50 dark:border-none dark:bg-neutral-800",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
+      <div className="relative flex items-center [&>svg:first-child]:absolute [&>svg:first-child]:left-2 [&>svg:first-child]:top-1/2 [&>svg:first-child]:-translate-y-1/2 [&>svg:last-child]:absolute [&>svg:last-child]:right-2 [&>svg:last-child]:top-1/2 [&>svg:last-child]:-translate-y-1/2 [&>svg]:h-6 [&>svg]:w-6 [&>svg]:cursor-pointer">
+        {LeftIcon}
+        <input
+          type={type}
+          className={cn(
+            "ring-offset-background flex h-10 w-full rounded-sm border-2 border-neutral-100 bg-white px-4 py-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 disabled:cursor-not-allowed disabled:opacity-50 dark:border-none dark:bg-neutral-800",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {RightIcon}
+      </div>
     );
   }
 );
