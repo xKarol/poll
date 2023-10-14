2c882397e09a1feb2da7c5ffd3669afa3db4fac6
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-14 15:41:17 +0200

    Displaying analtics data in line chart

--- apps/web/features/dashboard/pages/statistics.tsx
+++ apps/web/features/dashboard/pages/statistics.tsx
@@ -1,75 +1,50 @@
+import dayjs from "dayjs";
 import React from "react";
 import {
-  BarChart,
-  Bar,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
   ResponsiveContainer,
+  LineChart,
+  Line,
 } from "recharts";
 
 import Heading from "../components/heading";
 import { useVotesAnalytics } from "../hooks";
 import { BaseLayout } from "../layouts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+// TODO change param type
+const sortData = (d: unknown[]) => {
+  return [
+    ...Array.from({ length: 12 }, (_, index) => ({
+      name: dayjs().set("M", index).format("MMM"),
+      totalVotes: 0,
+    })),
+    ...d.map((dd) => ({
+      // @ts-expect-error
+      name: dayjs(dd.time).format("MMM"),
+      totalVotes: d.filter(
+        // @ts-expect-error
+        (ddd) => dayjs(ddd.time).day() === dayjs(dd.time).day()
+      ).length,
+    })),
+  ];
+};
 
 const StatisticsPage = () => {
   const d = useVotesAnalytics();
   console.log(d.data);
+
   return (
     <BaseLayout>
       <Heading className="mb-5">Statistics</Heading>
       <ResponsiveContainer width="100%" height="100%">
-        <BarChart
+        <LineChart
           width={500}
           height={300}
-          data={data}
+          data={d.isSuccess ? sortData(d.data) : []}
           margin={{
             top: 5,
             right: 30,
@@ -81,9 +56,13 @@ const StatisticsPage = () => {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
+          <Line
+            type="monotone"
+            dataKey="totalVotes"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
       </ResponsiveContainer>
     </BaseLayout>
   );
