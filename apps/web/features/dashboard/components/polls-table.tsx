dfcf9657c29c00ec5e548c3dfeaa91443d19efc9
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-18 18:13:15 +0200

    Add delete poll dialog component

--- apps/web/features/dashboard/components/polls-table.tsx
+++ apps/web/features/dashboard/components/polls-table.tsx
@@ -1,11 +1,20 @@
 import { cn } from "@poll/lib";
 import type { Poll } from "@poll/prisma";
 import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
   Icon,
+  LoadingButton,
 } from "@poll/ui";
 import type { FetchNextPageOptions } from "@tanstack/react-query";
 import dayjs from "dayjs";
@@ -90,6 +99,7 @@ function PollItem({
   ...props
 }: PollItemsProps) {
   const [isCopied, setIsCopied] = useState(false);
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const [, copy] = useCopyToClipboard();
 
   const handleCopy = (e: Event) => {
@@ -98,60 +108,89 @@ function PollItem({
     copy(`${getBaseUrl()}${routes.poll(id)}`);
   };
 
+  console.log(openDeleteModal);
+
   return (
-    <div
-      className={cn(
-        "flex rounded-[4px] border border-neutral-200 p-4 text-sm transition-colors hover:bg-neutral-700/5 dark:border-neutral-700/50",
-        className
-      )}>
-      <Link
-        className="flex w-full items-center justify-between [&>*]:max-w-[150px] [&>*]:flex-1"
-        {...props}>
-        <span className="!max-w-none">{question}</span>
-        <span>{totalVotes}</span>
-        <div>
-          <span className="rounded-[2px] bg-neutral-200 p-1 text-xs dark:bg-neutral-800">
-            {isPublic ? "Public" : "Private"}
-          </span>
+    <>
+      <div
+        className={cn(
+          "flex rounded-[4px] border border-neutral-200 p-4 text-sm transition-colors hover:bg-neutral-700/5 dark:border-neutral-700/50",
+          className
+        )}>
+        <Link
+          className="flex w-full items-center justify-between [&>*]:max-w-[150px] [&>*]:flex-1"
+          {...props}>
+          <span className="!max-w-none">{question}</span>
+          <span>{totalVotes}</span>
+          <div>
+            <span className="rounded-[2px] bg-neutral-200 p-1 text-xs dark:bg-neutral-800">
+              {isPublic ? "Public" : "Private"}
+            </span>
+          </div>
+          <div>{dayjs(createdAt).format("DD.MM.YYYY h:mm")}</div>
+        </Link>
+        <div className="flex">
+          <DropdownMenu
+            onOpenChange={(open) => {
+              if (open && isCopied) {
+                setIsCopied(false);
+              }
+            }}>
+            <DropdownMenuTrigger asChild>
+              <button
+                type="button"
+                className="ml-auto rounded-[4px] p-1 transition-colors hover:bg-neutral-200 hover:dark:bg-neutral-800">
+                <Icon.MoreHorizontal className="h-5 w-5" />
+              </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              align="end"
+              onCloseAutoFocus={(e) => e.preventDefault()}>
+              <DropdownMenuItem className="space-x-2" onSelect={handleCopy}>
+                {isCopied ? (
+                  <Icon.Check className="h-4 w-4" />
+                ) : (
+                  <Icon.Copy className="h-4 w-4" />
+                )}
+                <span>Copy link</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="space-x-2">
+                <Icon.Share2 className="h-4 w-4" />
+                <span>Share</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="space-x-2 text-red-400 [&>*]:pointer-events-none"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setOpenDeleteModal(true);
+                }}>
+                <Icon.Trash2 className="h-4 w-4" />
+                <span>Delete poll</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-        <div>{dayjs(createdAt).format("DD.MM.YYYY h:mm")}</div>
-      </Link>
-      <div className="flex">
-        <DropdownMenu
-          onOpenChange={(open) => {
-            if (open && isCopied) {
-              setIsCopied(false);
-            }
-          }}>
-          <DropdownMenuTrigger asChild>
-            <button
-              type="button"
-              className="ml-auto rounded-[4px] p-1 transition-colors hover:bg-neutral-200 hover:dark:bg-neutral-800">
-              <Icon.MoreHorizontal className="h-5 w-5" />
-            </button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            align="end"
-            onCloseAutoFocus={(e) => e.preventDefault()}>
-            <DropdownMenuItem className="space-x-2" onSelect={handleCopy}>
-              {isCopied ? (
-                <Icon.Check className="h-4 w-4" />
-              ) : (
-                <Icon.Copy className="h-4 w-4" />
-              )}
-              <span>Copy link</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="space-x-2">
-              <Icon.Share2 className="h-4 w-4" />
-              <span>Share</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="space-x-2 text-red-400 [&>*]:pointer-events-none">
-              <Icon.Trash2 className="h-4 w-4" />
-              <span>Delete poll</span>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
-    </div>
+      <Dialog open={openDeleteModal} onOpenChange={setOpenDeleteModal}>
+        <DialogContent hideClose>
+          <DialogHeader>
+            <DialogTitle>Delete poll</DialogTitle>
+            <DialogDescription>
+              Are you sure you want to delete this poll?
+            </DialogDescription>
+          </DialogHeader>
+
+          <DialogFooter>
+            <DialogTrigger asChild>
+              <Button variant="text">Cancel</Button>
+            </DialogTrigger>
+            <LoadingButton variant="destructive" isLoading={false}>
+              <Icon.Trash2 />
+              <span>Delete poll</span>
+            </LoadingButton>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
