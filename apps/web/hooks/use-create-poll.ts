c6fae4f2f82ce59623a554e825f5ebe577b11ea3
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-18 19:00:42 +0200

    Invalidating queries after create poll

--- apps/web/hooks/use-create-poll.ts
+++ apps/web/hooks/use-create-poll.ts
@@ -1,7 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+import { pollKeys } from "../queries/poll";
+import { userKeys } from "../queries/user";
 import { createPoll } from "../services/api";
 
 export const useCreatePoll = () => {
-  return useMutation({ mutationFn: createPoll });
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: createPoll,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: pollKeys.all });
+      queryClient.invalidateQueries({ queryKey: userKeys.userPolls });
+    },
+  });
 };
