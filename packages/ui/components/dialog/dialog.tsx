6b2082b935664432ec6cf78647505937e97f0153
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-19 10:25:40 +0200

    Fix dialog footer buttons spacing

--- packages/ui/components/dialog/dialog.tsx
+++ packages/ui/components/dialog/dialog.tsx
@@ -74,7 +74,7 @@ const DialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 [&>*]:mb-2 sm:[&>*]:mb-0 [&>:first-child]:mb-0",
       className
     )}
     {...props}
