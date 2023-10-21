078c8527835bb38438f69a0625104eb6e95276a3
Karol <karolpiskorz8@gmail.com>
2023-10-21 17:40:52 +0200

    Displaying badge only when user voted

--- apps/web/pages/[pollId].tsx
+++ apps/web/pages/[pollId].tsx
@@ -182,17 +182,19 @@ const PollPage = () => {
                       <span>Private</span>
                     </Badge>
                   ) : null}
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <span className="relative !ml-4 flex h-2.5 w-2.5">
-                        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-300 opacity-75"></span>
-                        <span className="relative inline-flex h-2.5 w-2.5 rounded-full bg-green-400"></span>
-                      </span>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <p>Real-time data is updated every 5 seconds</p>
-                    </TooltipContent>
-                  </Tooltip>
+                  {isVoted ? (
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <span className="relative !ml-4 flex h-2.5 w-2.5">
+                          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-300 opacity-75"></span>
+                          <span className="relative inline-flex h-2.5 w-2.5 rounded-full bg-green-400"></span>
+                        </span>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Real-time data is updated every 5 seconds</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  ) : null}
                 </div>
               </div>
               <RadioGroup
