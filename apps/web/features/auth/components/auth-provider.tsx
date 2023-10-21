d04b8b0ad9b1551de05e02df7f4236d8eead924b
Karol <karolpiskorz8@gmail.com>
2023-10-21 16:08:43 +0200

    Add animate-pulse instead of loader for AuthProvider

--- apps/web/features/auth/components/auth-provider.tsx
+++ apps/web/features/auth/components/auth-provider.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@poll/lib";
 import { Icon, LoadingButton } from "@poll/ui";
 import React from "react";
 
@@ -12,13 +13,22 @@ const variantIcons: Record<Providers, JSX.Element> = {
   google: <Icon.Google />,
 };
 
-export default function AuthProvider({ variant, ...props }: AuthProviderProps) {
+export default function AuthProvider({
+  variant,
+  isLoading,
+  className,
+  ...props
+}: AuthProviderProps) {
   return (
     <LoadingButton
       isLoading={false}
       type="button"
       variant="secondary"
-      className="space-x-4 border border-neutral-200 dark:border-none dark:bg-neutral-700 dark:text-white hover:dark:bg-neutral-700/50"
+      className={cn(
+        "space-x-4 border border-neutral-200 dark:border-none dark:bg-neutral-700 dark:text-white hover:dark:bg-neutral-700/50",
+        isLoading && "animate-pulse opacity-75 [&_svg:last-child]:hidden",
+        className
+      )}
       {...props}>
       {variantIcons[variant]}
       <span>
