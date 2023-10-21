e10a47a601766e528c3bef1ad52c93d70b0efbe6
Karol <karolpiskorz8@gmail.com>
2023-10-21 15:32:05 +0200

    Fix themes cards hydration warning

--- apps/web/features/settings/pages/preferences-appearance.tsx
+++ apps/web/features/settings/pages/preferences-appearance.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "next-themes";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { RadioGroup, RadioGroupItem } from "../../../components/radio-group";
 import { SettingsHeader, ThemeCard } from "../components";
@@ -8,8 +8,6 @@ import { BaseLayout } from "../layouts";
 const themes = ["light", "dark", "system"] as const;
 
 export default function AccountGeneralPage() {
-  const { theme, setTheme } = useTheme();
-
   return (
     <BaseLayout>
       <SettingsHeader
@@ -17,27 +15,40 @@ export default function AccountGeneralPage() {
         description="Manage your account preferences"
       />
       <div className="flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-        <RadioGroup
-          defaultValue={theme}
-          onValueChange={(theme) => setTheme(theme)}
-          asChild>
-          <>
-            {themes.map((themeName) => (
-              <ThemeCard
-                key={themeName}
-                variant={themeName}
-                isActive={theme === themeName}
-                RadioInput={
-                  <RadioGroupItem
-                    //   className="!border-neutral-700/50"
-                    value={themeName}
-                  />
-                }
-              />
-            ))}
-          </>
-        </RadioGroup>
+        <ThemesCards />
+        {/* TODO add sekeleton on server */}
       </div>
     </BaseLayout>
+  );
+}
+
+function ThemesCards() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <RadioGroup
+      defaultValue={theme}
+      onValueChange={(theme) => setTheme(theme)}
+      asChild>
+      <React.Fragment>
+        {themes.map((themeName) => (
+          <ThemeCard
+            key={themeName}
+            variant={themeName}
+            isActive={theme === themeName}
+            RadioInput={<RadioGroupItem value={themeName} />}
+          />
+        ))}
+      </React.Fragment>
+    </RadioGroup>
   );
 }
