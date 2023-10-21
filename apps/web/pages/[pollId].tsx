077088ef28a2ffe47560aef6137ead2fde795d3d
Karol <karolpiskorz8@gmail.com>
2023-10-21 16:50:45 +0200

    Add realtime updates indicator

--- apps/web/pages/[pollId].tsx
+++ apps/web/pages/[pollId].tsx
@@ -162,7 +162,7 @@ const PollPage = () => {
                 <h1 className="text-[22px] font-normal leading-[1.2] md:text-2xl xl:text-[32px]">
                   {data.question}
                 </h1>
-                <div className="flex flex-wrap space-x-2">
+                <div className="flex flex-wrap items-center space-x-2">
                   <span className="text-base font-normal text-neutral-400">
                     by a {data.user?.name || "guest"} ·{" "}
                     {dayjs(data.createdAt).fromNow()}
@@ -173,6 +173,10 @@ const PollPage = () => {
                       <span>Private</span>
                     </Badge>
                   ) : null}
+                  <span className="relative !ml-4 flex h-2.5 w-2.5">
+                    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-300 opacity-75"></span>
+                    <span className="relative inline-flex h-2.5 w-2.5 rounded-full bg-green-400"></span>
+                  </span>
                 </div>
               </div>
               <RadioGroup
