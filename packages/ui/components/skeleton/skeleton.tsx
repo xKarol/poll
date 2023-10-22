9fd5d2779fcc9ccbcbbc2600348af9131f5c5e10
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-22 19:02:37 +0200

    Fix skeleton bug for nested components

--- packages/ui/components/skeleton/skeleton.tsx
+++ packages/ui/components/skeleton/skeleton.tsx
@@ -7,7 +7,7 @@ function Skeleton({
   return (
     <div
       className={cn(
-        "animate-pulse rounded-[4px] bg-neutral-100 dark:bg-neutral-800 [&>div]:bg-neutral-200 [&>div]:dark:bg-neutral-700",
+        "animate-pulse rounded-[4px] bg-neutral-100 dark:bg-neutral-800 [&_div]:bg-neutral-200 [&_div]:dark:bg-neutral-700",
         className
       )}
       {...props}
