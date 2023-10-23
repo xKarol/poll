d1d145ca818b4775db9a67a09aeb548060e6f3a0
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-23 10:32:20 +0200

    Add top countries stats component

--- apps/web/features/dashboard/pages/statistics.tsx
+++ apps/web/features/dashboard/pages/statistics.tsx
@@ -59,36 +59,102 @@ const StatisticsPage = () => {
           Icon={<Icon.Share2 className="h-4 w-4" />}
         />
       </div>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={d.isSuccess ? sortData(d.data) : []}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis
-            dataKey="timestamp"
-            tickFormatter={(tick) => dayjs(tick).format("MMM")}
-            type="number"
-            scale="time"
-            domain={["dataMin", "dataMax"]}
-          />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="totalVotes"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+      <div className="flex h-full">
+        <div className="flex-1">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={d.isSuccess ? sortData(d.data) : []}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="timestamp"
+                tickFormatter={(tick) => dayjs(tick).format("MMM")}
+                type="number"
+                scale="time"
+                domain={["dataMin", "dataMax"]}
+              />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="totalVotes"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <section className="w-60 space-y-2">
+          <h1 className="text-sm font-medium">Top countries</h1>
+          <div className="flex flex-col space-y-1">
+            <Country
+              countryName="Poland"
+              countryFlag={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="10"
+                  id="Flag of Poland"
+                  viewBox="0 0 16 10">
+                  <rect width="16" height="10" fill="#fff" />
+                  <rect width="16" height="5" fill="#dc143c" y="5" />
+                </svg>
+              }
+              value={124}
+            />
+            <Country
+              countryName="USA"
+              countryFlag={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="10"
+                  viewBox="0 0 7410 3900">
+                  <rect width="7410" height="3900" fill="#b22234" />
+                  <path
+                    d="M0,450H7410m0,600H0m0,600H7410m0,600H0m0,600H7410m0,600H0"
+                    stroke="#fff"
+                    stroke-width="300"
+                  />
+                  <rect width="2964" height="2100" fill="#3c3b6e" />
+                  <g fill="#fff">
+                    <g id="s18">
+                      <g id="s9">
+                        <g id="s5">
+                          <g id="s4">
+                            <path
+                              id="s"
+                              d="M247,90 317.534230,307.082039 132.873218,172.917961H361.126782L176.465770,307.082039z"
+                            />
+                            <use y="420" />
+                            <use y="840" />
+                            <use y="1260" />
+                          </g>
+                          <use y="1680" />
+                        </g>
+                        <use x="247" y="210" />
+                      </g>
+                      <use x="494" />
+                    </g>
+                    <use x="988" />
+                    <use x="1976" />
+                    <use x="2470" />
+                  </g>
+                </svg>
+              }
+              value={87}
+            />
+          </div>
+        </section>
+      </div>
     </BaseLayout>
   );
 };
@@ -122,5 +188,31 @@ function StatisticCard({
       <h2 className="text-xl font-bold">{statsValue}</h2>
       <p className="text-xs text-neutral-400">{statsDifference}</p>
     </section>
+  );
+}
+
+type CountryProps = {
+  countryFlag: JSX.Element;
+  countryName: string;
+  value: number;
+} & React.ComponentPropsWithoutRef<"section">;
+
+function Country({ countryFlag, countryName, value, className }: CountryProps) {
+  return (
+    <div
+      className={cn(
+        "relative flex w-full items-center justify-between rounded-[4px] bg-neutral-100 px-4 py-2 text-xs",
+        className
+      )}>
+      <div className="flex items-center space-x-4">
+        {countryFlag}
+        <span>{countryName}</span>
+      </div>
+
+      <div className="flex items-center space-x-1">
+        <span className="font-medium">{value}</span>
+        <Icon.BarChart2 className="h-3 w-3" />
+      </div>
+    </div>
   );
 }
