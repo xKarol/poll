938e2a5b0285c2cc99f38ef6bf00eb42cd5f7348
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-25 18:28:05 +0200

    Use table component for my votes table

--- apps/web/features/dashboard/pages/my-votes.tsx
+++ apps/web/features/dashboard/pages/my-votes.tsx
@@ -1,8 +1,23 @@
-import { Icon } from "@poll/ui";
+import {
+  Badge,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  Icon,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@poll/ui";
 import dayjs from "dayjs";
+import { useRouter } from "next/router";
 import React from "react";
 
 import { InfiniteScrollContainer } from "../../../components/infinite-scroll-container";
+import { routes } from "../../../config/routes";
 import { getErrorMessage } from "../../../utils/error";
 import Heading from "../components/heading";
 import { useUserPollVotes } from "../hooks";
@@ -19,6 +34,7 @@ const MyVotesPage = () => {
     fetchNextPage,
   } = useUserPollVotes();
   const data = pages?.pages.flatMap(({ data }) => data);
+  const router = useRouter();
 
   return (
     <BaseLayout>
@@ -28,36 +44,74 @@ const MyVotesPage = () => {
         {isLoading && <Icon.Loader2 className="m-auto animate-spin" />}
         {isSuccess && (
           <>
-            <div className="mb-2 flex px-4 py-2 [&>*]:max-w-[150px] [&>*]:flex-1">
-              <span className="!max-w-none">Question</span>
-              <span className="!max-w-none">Answer option</span>
-              <span>Poll type</span>
-              <span>Voted Date</span>
-              <span className="space-x-2"></span>
-            </div>
             <InfiniteScrollContainer
               fetchNextPage={fetchNextPage}
               isFetchingNextPage={isFetchingNextPage}
               hasNextPage={hasNextPage}>
-              <div className="flex flex-col space-y-2">
-                {data?.map((vote) => (
-                  <div
-                    key={vote.id}
-                    className="flex items-center justify-between rounded-[4px] border border-neutral-700/50 px-4 py-2 text-sm transition-colors hover:bg-neutral-700/5 [&_*]:max-w-[150px] [&_*]:flex-1">
-                    <span className="!max-w-none">{vote.poll.question}</span>
-                    <span className="!max-w-none">{vote.answer.text}</span>
-                    <div>
-                      <span className="rounded-[2px] bg-neutral-800 p-1 text-xs">
-                        {vote.poll.isPublic ? "Public" : "Private"}
-                      </span>
-                    </div>
-                    <span>
-                      {dayjs(vote.createdAt).format("DD.MM.YYYY h:mm")}
-                    </span>
-                    <Icon.MoreHorizontal />
-                  </div>
-                ))}
-              </div>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Question</TableHead>
+                    <TableHead>Answer option</TableHead>
+                    <TableHead>Type</TableHead>
+                    <TableHead className="text-right">Date</TableHead>
+                    <TableHead></TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {data.map((vote) => (
+                    <TableRow key={vote.id} className="border-x">
+                      <TableCell className="max-w-xs truncate font-medium">
+                        {vote.poll.question}
+                      </TableCell>
+                      <TableCell className="max-w-xs truncate">
+                        {vote.answer.text}
+                      </TableCell>
+                      <TableCell>
+                        <Badge variant="secondary">
+                          {vote.poll.isPublic ? (
+                            <>
+                              <Icon.Globe />
+                              <span>Public</span>
+                            </>
+                          ) : (
+                            <>
+                              <Icon.Lock />
+                              <span>Private</span>
+                            </>
+                          )}
+                        </Badge>
+                      </TableCell>
+                      <TableCell className="truncate text-right">
+                        {dayjs(vote.createdAt).format("DD.MM.YYYY h:mm")}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <button
+                              type="button"
+                              className="ml-auto rounded-[4px] p-1 transition-colors hover:bg-neutral-200 hover:dark:bg-neutral-800">
+                              <Icon.MoreHorizontal className="h-5 w-5" />
+                            </button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent
+                            align="end"
+                            onCloseAutoFocus={(e) => e.preventDefault()}>
+                            <DropdownMenuItem
+                              className="space-x-2"
+                              onSelect={() =>
+                                router.push(routes.poll(vote.poll.id))
+                              }>
+                              <Icon.ExternalLink className="h-4 w-4" />
+                              <span>Open link</span>
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
             </InfiniteScrollContainer>
           </>
         )}
