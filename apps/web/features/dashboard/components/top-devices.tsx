cd2774749070cd53bf27f38bf362bf6e4b2a7f28
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-26 09:30:42 +0200

    Displaying top devices from api data

--- apps/web/features/dashboard/components/top-devices.tsx
+++ apps/web/features/dashboard/components/top-devices.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@poll/lib";
+import type { Analytics } from "@poll/types";
 import { Icon } from "@poll/ui";
 import React from "react";
 
@@ -6,47 +7,41 @@ import { useAnalyticsTopDevices } from "../hooks";
 
 type TopDevicesProps = React.ComponentPropsWithoutRef<"div">;
 
+const deviceIcon: Record<Analytics.Devices, JSX.Element> = {
+  mobile: <Icon.Smartphone className="h-4 w-4" />,
+  tablet: <Icon.Tablet className="h-4 w-4" />,
+  desktop: <Icon.Monitor className="h-4 w-4" />,
+};
+
 export default function TopDevices({ className, ...props }: TopDevicesProps) {
-  const topDevices = useAnalyticsTopDevices();
-  console.log(topDevices);
+  const { data, isSuccess, isError } = useAnalyticsTopDevices();
   return (
-    <div className={cn("w-60 space-y-2", className)} {...props}>
+    <div className={cn("flex w-60 flex-col space-y-2", className)} {...props}>
       <h1 className="text-sm font-medium">Top devices</h1>
-      <div className="flex flex-col space-y-1">
-        <div className="relative flex w-full items-center justify-between rounded-[4px] bg-neutral-100 px-4 py-2 text-xs dark:bg-neutral-800">
-          <div className="flex items-center space-x-4">
-            <Icon.Tablet className="h-4 w-4" />
-            <span>Tablet</span>
-          </div>
+      {isError && (
+        <span className="mx-auto text-xs text-neutral-400 dark:text-neutral-300">
+          Something went wrong...
+        </span>
+      )}
+      {isSuccess && (
+        <div className="flex flex-col space-y-1">
+          {Object.entries(data).map(([deviceName, total]) => (
+            <div
+              key={deviceName}
+              className="relative flex w-full items-center justify-between rounded-[4px] bg-neutral-100 px-4 py-2 text-xs dark:bg-neutral-800">
+              <div className="flex items-center space-x-4">
+                {deviceIcon[deviceName]}
+                <span className="capitalize">{deviceName}</span>
+              </div>
 
-          <div className="flex items-center space-x-1">
-            <span className="font-medium">100</span>
-            <Icon.BarChart2 className="h-3 w-3" />
-          </div>
+              <div className="flex items-center space-x-1">
+                <span className="font-medium">{total}</span>
+                <Icon.BarChart2 className="h-3 w-3" />
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="relative flex w-full items-center justify-between rounded-[4px] bg-neutral-100 px-4 py-2 text-xs dark:bg-neutral-800">
-          <div className="flex items-center space-x-4">
-            <Icon.Smartphone className="h-4 w-4" />
-            <span>Mobile</span>
-          </div>
-
-          <div className="flex items-center space-x-1">
-            <span className="font-medium">50</span>
-            <Icon.BarChart2 className="h-3 w-3" />
-          </div>
-        </div>
-        <div className="relative flex w-full items-center justify-between rounded-[4px] bg-neutral-100 px-4 py-2 text-xs dark:bg-neutral-800">
-          <div className="flex items-center space-x-4">
-            <Icon.Monitor className="h-4 w-4" />
-            <span>Desktop</span>
-          </div>
-
-          <div className="flex items-center space-x-1">
-            <span className="font-medium">150</span>
-            <Icon.BarChart2 className="h-3 w-3" />
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
