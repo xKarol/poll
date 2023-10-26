79b665791ca568ce3ffa95a6a5970886e7ebae94
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-26 10:43:47 +0200

    add getUserPollVotes api type

--- packages/types/analytics.d.ts
+++ packages/types/analytics.d.ts
@@ -6,7 +6,6 @@ export type Devices = "mobile" | "tablet" | "desktop";
 
 // Frontend
 export interface Api {
-  // TODO add type
-  getUserPollVotes: () => Promise<unknown[]>;
+  getUserPollVotes: () => Promise<{ timestamp: number; totalVotes: number }[]>;
   getUserPollTopDevices: () => Promise<Record<Devices, number>>;
 }
