4df4503233190b34a6da9d724e3fbcf6e5e60c31
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-29 12:19:18 +0100

    Add scroll area component

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -18,3 +18,4 @@ export * from "./components/tooltip";
 export * from "./components/avatar";
 export * from "./components/table";
 export * from "./components/separator";
+export * from "./components/scroll-area";
