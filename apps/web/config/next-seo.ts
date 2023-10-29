433c51675c8e62bd1586eed7d1272679c15be53d
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-29 11:15:02 +0100

    Add theme color

--- apps/web/config/next-seo.ts
+++ apps/web/config/next-seo.ts
@@ -3,6 +3,7 @@ import type { DefaultSeoProps } from "next-seo";
 const nextSeoConfig: DefaultSeoProps = {
   defaultTitle: "Poll",
   titleTemplate: "Poll - %s",
+  themeColor: "#000",
 };
 
 export default nextSeoConfig;
