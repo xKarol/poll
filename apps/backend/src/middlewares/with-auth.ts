f47a250733da28b2d99eccf3bf4f8b4fc5ecdaeb
Karol Piskorz <karolpiskorz8@gmail.com>
2023-10-31 16:59:17 +0100

    Remove debug logs in withAuth middleware

--- apps/backend/src/middlewares/with-auth.ts
+++ apps/backend/src/middlewares/with-auth.ts
@@ -20,15 +20,12 @@ export const withAuth = async (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("DEBUG_COOKIES:", req.headers.cookie);
-  console.log("DEBUG_COOKIE_NAME:", `${cookiePrefix}next-auth.session-token`);
   const jwtData = (await decode({
     secret: process.env.NEXTAUTH_SECRET as string,
     token: cookie.parse(req.headers.cookie || "")[
       `${cookiePrefix}next-auth.session-token`
     ],
   })) as Auth.JWTPayload | null;
-  console.log("DEBUG_JWT_DATA:", jwtData);
   req.user = jwtData;
   next();
 };
