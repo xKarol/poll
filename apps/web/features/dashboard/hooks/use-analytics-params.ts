a4e07b2386945e8ea12a97765fc76543e7a89aa4
Karol <karolpiskorz8@gmail.com>
2023-11-02 19:48:11 +0100

    Minor refactor useAnalyticsParams hook

--- apps/web/features/dashboard/hooks/use-analytics-params.ts
+++ apps/web/features/dashboard/hooks/use-analytics-params.ts
@@ -4,17 +4,10 @@ import { useQueryState } from "next-usequerystate";
 
 const intervals = {
   "1h": [1, "hour"],
-  "1d": [1, "day"],
+  "24h": [24, "hour"],
   "7d": [7, "day"],
   "30d": [30, "day"],
 } as const;
-
-const convertIntervals = {
-  "1h": "1h",
-  "1d": "24h",
-  "7d": "7d",
-  "30d": "30d",
-};
 
 export default function useAnalyticsQueryParams() {
   const [interval] = useQueryState("interval");
@@ -30,11 +23,10 @@ export default function useAnalyticsQueryParams() {
 }
 
 function calculateDate(value: string) {
-  const convertedValue = convertIntervals[value];
-  if (convertedValue === "1h") return dateDiff(1, "hour");
-  if (convertedValue === "24h") return dateDiff(24, "hour");
-  if (convertedValue === "7d") return dateDiff(7, "day");
-  if (convertedValue === "30d") return dateDiff(30, "day");
+  if (value === "1h") return dateDiff(1, "hour");
+  if (value === "24h") return dateDiff(24, "hour");
+  if (value === "7d") return dateDiff(7, "day");
+  if (value === "30d") return dateDiff(30, "day");
   return dateDiff(24, "h");
 }
 
