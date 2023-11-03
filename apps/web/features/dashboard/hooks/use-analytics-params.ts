630244a8273eef8c3949d483290526c73145b5c8
Karol <karolpiskorz8@gmail.com>
2023-11-03 18:24:19 +0100

    Ignore typescript error

--- apps/web/features/dashboard/hooks/use-analytics-params.ts
+++ apps/web/features/dashboard/hooks/use-analytics-params.ts
@@ -6,6 +6,7 @@ import type { Analytics } from "@poll/types";
 import dayjs from "dayjs";
 import { useQueryState } from "next-usequerystate";
 
+// @ts-ignore TODO fix
 const intervals: Record<Analytics.Interval, [number, Analytics.GroupBy]> = {
   "1h": [60, "minute"],
   "24h": [24, "hour"],
