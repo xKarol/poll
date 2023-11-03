f2a621f9f83f2b319e4dba0e2f1093dedc73ce98
Karol <karolpiskorz8@gmail.com>
2023-11-03 09:59:58 +0100

    Add eslint plugin tailwindcss

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -5,6 +5,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:import/recommended",
     "plugin:import/typescript",
+    "plugin:tailwindcss/recommended",
     "plugin:prettier/recommended",
     "turbo",
   ],
