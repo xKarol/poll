e564ffb4ec15a75cb798bfe073e29a032e86b2a5
Karol Piskorz <karolpiskorz8@gmail.com>
2023-11-07 15:20:52 +0100

    Add create-poll e2e test (need to fix)

---
+++ apps/web/playwright/create-poll.e2e.ts
@@ -0,0 +1,37 @@
+import { expect } from "@playwright/test";
+
+import { routes } from "../config/routes";
+import { test } from "./fixtures";
+
+test.afterEach(({ poll }) => poll.deleteAll());
+
+test.describe("Create Poll", () => {
+  test.fixme("should create poll", async ({ page, prisma }) => {
+    await page.goto(routes.CREATE_POLL);
+
+    await page.getByPlaceholder("Your question...").fill("test question");
+    await page.getByPlaceholder("Option 1").fill("test option 1");
+    await page.getByPlaceholder("Option 2").fill("test option 2");
+    await page.getByRole("button", { name: "Add option" }).click();
+    await page.getByPlaceholder("Option 3").fill("test option 3");
+    await page.getByRole("switch").first().click();
+    await page.getByRole("button", { name: "Create" }).click(),
+      // TODO wait for success toast
+
+      await page
+        .getByRole("button", { name: "Create", disabled: false })
+        .waitFor();
+
+    const pollData = await prisma.poll.findFirstOrThrow({
+      include: { answers: true },
+    });
+
+    expect(pollData.answers.length).toBe(3);
+    expect(pollData.isPublic).toBeFalsy();
+    expect(pollData.question).toBe("test question");
+
+    pollData.answers.forEach((answer, index) => {
+      expect(answer.text).toBe(`test option ${index + 1}`);
+    });
+  });
+});
