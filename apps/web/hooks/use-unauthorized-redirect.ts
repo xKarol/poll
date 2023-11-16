36975494f6e1c192d1eb87addc449322327468c5
Karol Piskorz <karolpiskorz8@gmail.com>
2023-11-16 13:21:16 +0100

    Redirect to previous page in useUnauthorizedRedirect hook

--- apps/web/hooks/use-unauthorized-redirect.ts
+++ apps/web/hooks/use-unauthorized-redirect.ts
@@ -3,12 +3,14 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 import { routes } from "../config/routes";
+import { getBaseUrl } from "../utils/get-base-url";
 
-export const useUnauthorizedRedirect = (
-  redirectPath: string = routes.LOGIN
-) => {
+export const useUnauthorizedRedirect = (redirectPath: string | undefined) => {
   const { status } = useSession();
   const router = useRouter();
+
+  redirectPath =
+    redirectPath ?? `${routes.LOGIN}?redirect=${getBaseUrl()}${router.asPath}`;
 
   useEffect(() => {
     if (status === "unauthenticated") {
