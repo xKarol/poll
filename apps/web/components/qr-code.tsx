cd384696612c2d1fc885a0bc7b7bdc2cbf28578e
Karol Piskorz <karolpiskorz8@gmail.com>
2023-11-24 17:51:05 +0100

    Add QRCode component

---
+++ apps/web/components/qr-code.tsx
@@ -0,0 +1,53 @@
+import { cn } from "@poll/lib";
+import { Button, Icon } from "@poll/ui";
+import Image from "next/image";
+import React from "react";
+
+import { useQRCode } from "../hooks/use-qr-code";
+
+type QRCodeProps = { text: string } & Omit<
+  React.ComponentPropsWithoutRef<"div">,
+  "children"
+>;
+
+export default function QRCode({ text, className, ...props }: QRCodeProps) {
+  const {
+    data: qrCodeSrc,
+    isLoading,
+    isError,
+    refetch: refetchQRCode,
+  } = useQRCode(text);
+
+  return (
+    <div
+      className={cn(
+        "flex h-40 w-40 items-center justify-center overflow-hidden rounded border-2 border-neutral-200 dark:border-neutral-800",
+        className
+      )}
+      {...props}>
+      {isLoading && (
+        <Icon.Loader2 className="h-6 w-6 animate-spin text-neutral-500" />
+      )}
+      {isError && (
+        <div className="flex flex-col items-center space-y-2">
+          <Icon.X className="h-6 w-6 text-red-500" />
+          <Button
+            onClick={() => refetchQRCode()}
+            className="text-xs"
+            size="sm"
+            variant="text">
+            Try Again
+          </Button>
+        </div>
+      )}
+      {qrCodeSrc ? (
+        <Image
+          width={160}
+          height={160}
+          src={Buffer.from(qrCodeSrc).toString()}
+          alt="poll share qr code"
+        />
+      ) : null}
+    </div>
+  );
+}
--- apps/web/features/dashboard/components/share-poll-dialog.tsx
+++ apps/web/features/dashboard/components/share-poll-dialog.tsx
@@ -1,15 +1,12 @@
 import {
-  Button,
   Dialog,
   DialogContent,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
   Icon,
   Separator,
 } from "@poll/ui";
-import Image from "next/image";
 import React from "react";
 import {
   EmailShareButton,
@@ -21,9 +18,9 @@ import {
   WhatsappShareButton,
 } from "react-share";
 
+import QRCode from "../../../components/qr-code";
 import { ShareSocial } from "../../../components/share-social";
 import { routes } from "../../../config/routes";
-import { useQRCode } from "../../../hooks/use-qr-code";
 
 type DeletePollDialogProps = {
   pollId: string;
@@ -39,13 +36,6 @@ export default function SharePollDialog({
       ? ""
       : `${window.location.origin}${routes.poll(pollId)}`;
 
-  const {
-    data: qrCodeSrc,
-    isLoading,
-    isError,
-    refetch: refetchQRCode,
-  } = useQRCode(shareUrl);
-
   return (
     <Dialog {...props}>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -57,31 +47,7 @@ export default function SharePollDialog({
         </DialogHeader>
 
         <div className="mb-4 mt-8 flex w-full flex-col items-center">
-          <div className="flex h-40 w-40 items-center justify-center rounded border-2 border-neutral-200 p-0.5 dark:border-neutral-800">
-            {isLoading && (
-              <Icon.Loader2 className="h-6 w-6 animate-spin text-neutral-500" />
-            )}
-            {isError && (
-              <div className="flex flex-col items-center space-y-2">
-                <Icon.X className="h-6 w-6 text-red-500" />
-                <Button
-                  onClick={() => refetchQRCode()}
-                  className="text-xs"
-                  size="sm"
-                  variant="text">
-                  Try Again
-                </Button>
-              </div>
-            )}
-            {qrCodeSrc ? (
-              <Image
-                width={160}
-                height={160}
-                src={Buffer.from(qrCodeSrc).toString()}
-                alt="poll share qr code"
-              />
-            ) : null}
-          </div>
+          <QRCode text={shareUrl} />
           <Separator className="my-12" />
           <div className="flex flex-wrap gap-4">
             <ShareSocial as={EmailShareButton} url={shareUrl}>
@@ -107,12 +73,6 @@ export default function SharePollDialog({
             </ShareSocial>
           </div>
         </div>
-
-        <DialogFooter>
-          <DialogTrigger asChild>
-            <Button variant="text">Cancel</Button>
-          </DialogTrigger>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
