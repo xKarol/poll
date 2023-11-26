47fe487ac482f7087b85da25676d31eee171c9d5
Karol Piskorz <karolpiskorz8@gmail.com>
2023-11-26 13:43:28 +0100

    Add sentry for frontend

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require("@sentry/nextjs");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
   async rewrites() {
@@ -16,3 +20,12 @@ module.exports = {
     };
   },
 };
+
+const sentryWebpackPluginOptions = {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: true,
+  hideSourceMaps: true,
+};
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
