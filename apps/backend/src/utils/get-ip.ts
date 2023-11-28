8df897655fe4b817c1fe4f3a65bf993bb636e12e
Karol Piskorz <karolpiskorz8@gmail.com>
2023-11-28 14:01:30 +0100

    Change getIp function headers

--- apps/backend/src/utils/get-ip.ts
+++ apps/backend/src/utils/get-ip.ts
@@ -2,6 +2,6 @@ import type { Request } from "express";
 
 export const getIP = (req: Request) => {
   const ip =
-    req.headers["x-forwarded-for"] || req.headers["true-client-ip"] || req.ip;
+    req.headers["true-client-ip"] || req.headers["x-real-ip"] || req.ip;
   return ip as string;
 };
