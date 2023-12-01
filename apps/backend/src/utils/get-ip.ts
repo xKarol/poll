7b5325aacf2296ff9c7894c3b0c990754da56842
Karol Piskorz <karolpiskorz8@gmail.com>
2023-12-01 20:36:07 +0100

    Get client ip from different req header

--- apps/backend/src/utils/get-ip.ts
+++ apps/backend/src/utils/get-ip.ts
@@ -2,6 +2,9 @@ import type { Request } from "express";
 
 export const getIP = (req: Request) => {
   const ip =
-    req.headers["true-client-ip"] || req.headers["x-real-ip"] || req.ip;
+    req.headers["x-forwarded-for"] ||
+    req.headers["true-client-ip"] ||
+    req.headers["x-real-ip"] ||
+    req.ip;
   return ip as string;
 };
