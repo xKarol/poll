4f0a781ccff2688e472b8f0d63f3ce721184fa7e
Karol Piskorz <karolpiskorz8@gmail.com>
2023-12-06 18:42:17 +0100

    Refactor useUnauthorizedRedirect hook

--- apps/web/hooks/use-unauthorized-redirect.ts
+++ apps/web/hooks/use-unauthorized-redirect.ts
@@ -1,22 +1,20 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 import { routes } from "../config/routes";
 import { getBaseUrl } from "../utils/get-base-url";
 
 export const useUnauthorizedRedirect = (redirectPath?: string) => {
-  const { status } = useSession();
   const router = useRouter();
-
   redirectPath =
     redirectPath ?? `${routes.LOGIN}?redirect=${getBaseUrl()}${router.asPath}`;
 
-  useEffect(() => {
-    if (status === "unauthenticated") {
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
       router.replace(redirectPath);
-    }
-  }, [status, redirectPath, router]);
+    },
+  });
 
   return {
     isLoading: status === "loading",
