a4c87137c378e8c5baa376b35543a8195e14b102
Karol Piskorz <karolpiskorz8@gmail.com>
2023-12-07 19:19:23 +0100

    Add download qr code option

--- apps/web/features/dashboard/components/share-poll-dialog.tsx
+++ apps/web/features/dashboard/components/share-poll-dialog.tsx
@@ -5,9 +5,13 @@ import {
   DialogTitle,
   DialogTrigger,
   Icon,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
   Separator,
 } from "@poll/ui";
-import React from "react";
+import React, { useRef } from "react";
 import {
   EmailShareButton,
   FacebookShareButton,
@@ -21,6 +25,7 @@ import {
 import QRCode from "../../../components/qr-code";
 import { ShareSocial } from "../../../components/share-social";
 import { routes } from "../../../config/routes";
+import { useQRCode } from "../../../hooks/use-qr-code";
 import { getBaseUrl } from "../../../utils/get-base-url";
 
 type DeletePollDialogProps = {
@@ -33,6 +38,16 @@ export default function SharePollDialog({
   ...props
 }: DeletePollDialogProps) {
   const shareUrl = `${getBaseUrl()}${routes.poll(pollId)}`;
+  const { data: qrCodeSrc } = useQRCode(shareUrl);
+  const downloadLinkRef = useRef<HTMLAnchorElement>(null);
+
+  const downloadQRCode = (type: string) => {
+    const downloadEl = downloadLinkRef.current;
+    if (!downloadEl) return;
+    downloadEl.href = qrCodeSrc;
+    downloadEl.download = `qr.${type}`;
+    downloadEl.click();
+  };
 
   return (
     <Dialog {...props}>
@@ -45,7 +60,22 @@ export default function SharePollDialog({
         </DialogHeader>
 
         <div className="mb-4 mt-8 flex w-full flex-col items-center">
-          <QRCode text={shareUrl} />
+          <QRCode
+            className="rounded border-2 border-neutral-200 dark:border-neutral-800"
+            text={shareUrl}
+          />
+          <div className="mt-4 flex space-x-4">
+            <Select onValueChange={downloadQRCode}>
+              <SelectTrigger>
+                <Icon.Download className="h-4 w-4" />
+                <span>Download</span>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="png">PNG</SelectItem>
+                <SelectItem value="jpeg">JPEG</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
           <Separator className="my-12" />
           <div className="flex flex-wrap gap-4">
             <ShareSocial as={EmailShareButton} url={shareUrl}>
@@ -71,6 +101,7 @@ export default function SharePollDialog({
             </ShareSocial>
           </div>
         </div>
+        <a ref={downloadLinkRef} hidden />
       </DialogContent>
     </Dialog>
   );
