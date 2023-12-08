10647c4650fe5ce7306141a3d238f4f712bf6fd6
Karol Piskorz <karolpiskorz8@gmail.com>
2023-12-08 14:52:16 +0100

    Add meta og image tag

--- apps/web/config/next-seo.ts
+++ apps/web/config/next-seo.ts
@@ -4,6 +4,20 @@ const nextSeoConfig: DefaultSeoProps = {
   defaultTitle: "Quick Poll",
   titleTemplate: "%s | Quick Poll",
   themeColor: "#000",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://poll-web-three.vercel.app",
+    siteName: "Quick Poll",
+    images: [
+      {
+        url: "https://poll-web-three.vercel.app/logo-banner.png",
+        width: 1200,
+        height: 630,
+        alt: "quick poll logo",
+      },
+    ],
+  },
 };
 
 export default nextSeoConfig;
