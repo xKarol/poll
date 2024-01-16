cc1ff3da3d4511fa1bfdfc404aa38d36c19e9594
Karol Piskorz <karolpiskorz8@gmail.com>
2024-01-16 13:26:36 +0100

    capture some prisma errors

--- apps/backend/src/utils/capture-error.ts
+++ apps/backend/src/utils/capture-error.ts
@@ -1,9 +1,15 @@
+import { Prisma } from "@poll/prisma";
 import httpError from "http-errors";
 import { ZodError } from "zod";
 
 export const captureError = (error: unknown) => {
   if (error instanceof ZodError)
     return httpError.Forbidden(error.issues[0].message);
+
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    return httpError(400, error.meta?.cause || "Unknown database error");
+  }
+
   if (error instanceof httpError.HttpError) {
     return httpError(error);
   }
